b02f82617efa02c02a889d91cf0f502e43a5a524
yuu <yuu@users.noreply.github.com>
2017-07-24 13:41:24 +0900

    go doc

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
+// main Package
+//
 package main
 
 import "fmt"
 
+// return value: i + j
+func Add(i, j int) int {
+	return i + j
+}
+
+// endpoint
 func main() {
 	fmt.Printf("Hello, go!!\n")
 }
